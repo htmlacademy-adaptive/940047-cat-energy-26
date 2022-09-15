72da95035ccee6ebdf3db072e1c784f88e1b9c38
Anastasia Kulaeva <kulaeva.anastasia1995@yandex.ru>
2022-09-15 20:09:11 +0300

    task 8 svg

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
 
 // Styles
 
@@ -14,16 +19,68 @@ export const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+
+// HTML
+export const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+}
+
+// Scripts
+export const scripts = () => {
+  return gulp.src('source/js/*.js')
+  .pipe(terser())
+  .pipe(gulp.dest('build/js'));
+}
+
+//Images
+const optimizeImages = () => {
+  return gulp.src ('source/images/**/*.{jpg,png}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/images'));
+}
+
+const copyImages = () => {
+  return gulp.src ('source/images/**/*.{jpg,png}')
+  .pipe(gulp.dest('build/images'));
+}
+
+// WebP
+export const createWebp = () => {
+  return gulp.src('source/images/**/*.{jpg,png}')
+  .pipe(squoosh({
+    webp: {}
+  }))
+  .pipe(gulp.dest('build/images'));
+}
+
+//SVG
+export const svg = () => {
+  gulp.src(['source/images/*.svg', 'source/images/decoration.svg', 'source/images/favicons.svg', 'source/images/icon.svg', 'source/images/numbers.svg', '!source/images/sprite.svg'])
+  .pipe(svgo())
+  .pipe(gulp.dest('build/images'));
+}
+
+export const sprite = () => {
+  return gulp.src('source/images/sprite.svg')
+  .pipe(svgo())
+  .pipe(svgstore({
+    inlineSvg: true
+  }));
+}
+
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +98,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html, styles, server, watcher
 );
